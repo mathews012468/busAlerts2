5f5661cc2ee857dced0f0d8ec6a1f4a76a6f2ae4
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-02-27 15:22:46 -0500

    configure logging and change working directory

--- busAlertsBackend/api.py
+++ busAlertsBackend/api.py
@@ -4,9 +4,18 @@ from flask import render_template
 import busAlertScraper as bas
 from multiprocessing import Process
 import re
+import logging
+from datetime import date
 import os
 
 app = Flask(__name__)
+#How to make log messages from other libraries not appear: https://stackoverflow.com/a/8269542
+#the idea is to set the logging level for other libraries higher than for my code
+#another answer on that post says that's not ideal, since it doesn't really stop
+#the other libraries from logging, but it's good enough for me.
+logging.basicConfig(filename=f'/app/logs/log', level=logging.ERROR, format='%(asctime)s %(name)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 @app.route('/', methods=["GET"])
 def entry():
