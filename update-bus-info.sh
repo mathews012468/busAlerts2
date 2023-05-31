4de988c06136c67fe100318c38da6cdfc67f6e72
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-05-30 23:50:56 -0400

    admit that I will run the refresh script in dev and not in prod

--- update-bus-info.sh
+++ update-bus-info.sh
@@ -3,7 +3,6 @@
 set -o allexport && source .env && set +o allexport
 cd busAlertsBackend
 git checkout main
-python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python refreshBusData.py
@@ -13,7 +12,3 @@ git push 'https://'$GITUSER':'$TOKEN'@github.com/'$GITUSER'/busAlerts2.git' main
 git checkout prod
 git merge main --commit -m "update bus info in prod"
 git push 'https://'$GITUSER':'$TOKEN'@github.com/'$GITUSER'/busAlerts2.git' prod
-cd ..
-sudo docker-compose down
-sudo docker-compose build
-sudo docker-compose up -d
