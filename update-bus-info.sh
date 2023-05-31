9fd9636b9a7519ec7b3c6700bacb66986f78c6a4
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-05-30 23:30:39 -0400

    update info script

---
+++ update-bus-info.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+set -o allexport && source .env && set +o allexport
+cd busAlertsBackend
+git checkout main
+python refreshBusData.py
+git add .
+git commit -m "refresh bus data"
+git push 'https://'$GITUSER':'$TOKEN'@github.com/'$GITUSER'/busAlerts2.git' main
+git checkout prod
+git merge main --commit -m "update bus info in prod"
+git push 'https://'$GITUSER':'$TOKEN'@github.com/'$GITUSER'/busAlerts2.git' prod
+cd ..
+sudo docker-compose down
+sudo docker-compose build
+sudo docker-compose up -d
