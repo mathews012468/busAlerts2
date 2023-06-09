31255e0979e2e1ea46a7193305e9b3fc19846906
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-06-09 16:09:13 -0400

    update bus info refresh script

--- update-bus-info.sh
+++ update-bus-info.sh
@@ -12,3 +12,4 @@ git push 'https://'$GITUSER':'$TOKEN'@github.com/'$GITUSER'/busAlerts2.git' main
 git checkout prod
 git merge main --commit -m "update bus info in prod"
 git push 'https://'$GITUSER':'$TOKEN'@github.com/'$GITUSER'/busAlerts2.git' prod
+git pull
